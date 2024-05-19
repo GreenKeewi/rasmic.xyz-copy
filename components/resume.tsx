50e302198f9bef9991a2931eee66d2f2e84d4d31
GreenKeewi <aliononmars07@gmail.com>
2024-05-19 16:37:15 -0400

    added context

--- components/resume.tsx
+++ components/resume.tsx
@@ -3,26 +3,50 @@ const Resume = () => {
     <div>
       <p>In Progress</p>
       <div className="grid grid-cols-8 grid-rows-8 gap-3">
-        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8"></div>
-        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8"></div>
-        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8"></div>
-        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8"></div>
+        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8 text-xl">
+          Lorem, ipsum.
+        </div>
+        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8 text-xl">
+          Lorem, ipsum.
+        </div>
+        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8 text-xl">
+          Lorem, ipsum.
+        </div>
+        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8 text-xl">
+          Lorem, ipsum.
+        </div>
       </div>
       <br />
       <p>My Experience</p>
       <div className="grid grid-cols-8 grid-rows-8 gap-3">
-        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8"></div>
-        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8"></div>
-        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8"></div>
-        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8"></div>
+        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8 text-xl">
+          Lorem, ipsum.
+        </div>
+        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8 text-xl">
+          Lorem, ipsum.
+        </div>
+        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8 text-xl">
+          Lorem, ipsum.
+        </div>
+        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8 text-xl">
+          Lorem, ipsum.
+        </div>
       </div>
       <br />
       <p>Freelance Work</p>
       <div className="grid grid-cols-8 grid-rows-8 gap-3">
-        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8"></div>
-        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8"></div>
-        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8"></div>
-        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8"></div>
+        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8 text-xl">
+          Lorem, ipsum.
+        </div>
+        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8 text-xl">
+          Lorem, ipsum.
+        </div>
+        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8 text-xl">
+          Lorem, ipsum.
+        </div>
+        <div className="p-2 shadow-[0_8px_32px_0_rgba(_31,38,135,0.37_)] backdrop-blur-[_9px_] border rounded-[10px] border-solid border-[rgba(_255,255,255,0.18_)]; col-span-4 row-span-8 text-xl">
+          Lorem, ipsum.
+        </div>
       </div>
     </div>
   );
